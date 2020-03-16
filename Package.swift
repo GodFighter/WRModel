14226da5dfd96f88d27e7128e3556391d909ab4a
{GpdFighter} <{xianghui_ios@163.com}>
2020-03-16 10:52:31 +0800

    SPM文件

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "WRModel",
+    products: [
+        .library(name: "WRModel", targets: ["WRModel"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ccgus/fmdb.git", from: "2.7.4"),
+        .package(url: "https://github.com/kakaopensource/KakaJSON.git", from: "1.1.2"),
+    ],
+    targets: [
+        .target(name: "WRModel", path: "WRModel/Classes")
+    ]
+)
